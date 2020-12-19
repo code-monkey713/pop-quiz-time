57adadc9cca1eb4a58a6f7008e1d3bb228c2e876
Tony Tran <codemonkey713@gmail.com>
2020-12-18 19:33:05 -0600

    able to add the elements and possible answers when the start button is clicked

--- script2.js
+++ script2.js
@@ -24,7 +24,7 @@ function initialState() {
   startBtn.setAttribute('class', 'btn btn-primary startBtn');
   startBtn.setAttribute('id', 'startBtn');
   startBtn.appendChild(textBtn);
-  console.log(startBtn);
+  //console.log(startBtn);
   mainEl.appendChild(startBtn);
 }
 
@@ -51,9 +51,53 @@ function setTimer() {
   }, 1000);
 }
 
+function setQuestions () {
+  // console.log('This is setting the questions.');
+  const question = document.createElement('h1');
+  const text = document.createTextNode('This is question 1!');
+  mainEl.appendChild(text);
+
+  const lineOne = document.createElement('p');
+  mainEl.appendChild(lineOne);
+
+  const answer1Btn = document.createElement('button');
+  const answer1Text = document.createTextNode('Answer one to question!');
+  //answer1Btn = setAttribute('id', 'answer1');
+  answer1Btn.appendChild(answer1Text);
+  mainEl.appendChild(answer1Btn);
+
+  const lineTwo = document.createElement('p');
+  mainEl.appendChild(lineTwo);
+
+  const answer2Btn = document.createElement('button');
+  const answer2Text = document.createTextNode('Answer two to question!');
+  //answer1Btn = setAttribute('id', 'answer1');
+  answer2Btn.appendChild(answer2Text);
+  mainEl.appendChild(answer2Btn);
+
+  const lineThree = document.createElement('p');
+  mainEl.appendChild(lineThree);
+
+  const answer3Btn = document.createElement('button');
+  const answer3Text = document.createTextNode('Answer three to question!');
+  //answer1Btn = setAttribute('id', 'answer1');
+  answer3Btn.appendChild(answer3Text);
+  mainEl.appendChild(answer3Btn);
+
+  const lineFour = document.createElement('p');
+  mainEl.appendChild(lineFour);
+
+  const answer4Btn = document.createElement('button');
+  const answer4Text = document.createTextNode('Answer four to question!');
+  //answer1Btn = setAttribute('id', 'answer1');
+  answer4Btn.appendChild(answer4Text);
+  mainEl.appendChild(answer4Btn);
+}
+
 function popQuiz() {
   clearContent();
-  setTimer();
+  //setTimer();
+  setQuestions();
 }
 
 startBtn.addEventListener('click', popQuiz);
